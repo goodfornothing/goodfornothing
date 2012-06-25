0e6a3da5dd28457acabe6678617c550cf2cacd4a
Andrew Sprinz <andrew@phenotypic.co.uk>
2012-06-25 12:54:09 +0100

    Published bool for tags

--- app/controllers/library/bookmarks_controller.rb
+++ app/controllers/library/bookmarks_controller.rb
@@ -2,12 +2,12 @@ class Library::BookmarksController < ApplicationController
 
 	def index
 	  page = params[:page] || 1
-		@bookmarks = Library::Bookmark.order("created_at DESC").where("published = 1").page(page)
+		@bookmarks = Library::Bookmark.order("created_at DESC").where("published = true").page(page)
 		@tags = Library::Tag.all
 	end
 
 	def search
-	  @bookmarks = Library::Bookmark.where('title like ?',"%#{params[:term]}%").where("published = 1")
+	  @bookmarks = Library::Bookmark.where('title like ?',"%#{params[:term]}%").where("published = true")
 	  @tags = Library::Tag.all
 	end
 

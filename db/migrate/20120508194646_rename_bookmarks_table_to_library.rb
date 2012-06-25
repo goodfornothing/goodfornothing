796f595eb1974ac8f187fbde1ac3ebd31c3137d0
Andrew Sprinz <andrew@phenotypic.co.uk>
2012-06-25 12:44:36 +0100

    Migration tidy

--- db/migrate/20120508194646_rename_bookmarks_table_to_library.rb
+++ db/migrate/20120508194646_rename_bookmarks_table_to_library.rb
@@ -1,12 +1,12 @@
 class RenameBookmarksTableToLibrary < ActiveRecord::Migration
   def up
-  	rename_column :bookmarks, :bookmark_tag_id, :library_tag_id
+  	rename_column :bookmarks, :bookmark_tags_id, :library_tag_id
   	rename_table :bookmarks, :library_bookmarks
   	rename_table :bookmark_tags, :library_tags
   end
 
   def down
-  	rename_column :bookmarks, :library_tag_id, :bookmark_tag_id
+  	rename_column :bookmarks, :library_tag_id, :bookmark_tags_id
 		rename_table :library_bookmarks, :bookmarks
   	rename_table :library_tags, :bookmark_tags
   end

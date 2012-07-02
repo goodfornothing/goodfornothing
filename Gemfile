83de7c5f5ad0ac3923e24be9e34f45e5dc930799
Andrew Sprinz <andrew@phenotypic.co.uk>
2012-07-02 16:07:23 +0100

    s3

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'meta_search', '>= 1.1.0.pre'
 gem 'sass-rails'
 
 gem 'friendly_id', '~> 4.0.1'
+gem "fog", "~> 1.3.1"
 
 # Visualising domain model
 gem 'railroady'
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,3 @@
-CarrierWave.configure do |config| 
-  config.fog_credentials = { 
-    :provider               => 'AWS', 
-    :aws_access_key_id      => 'AKIAIWNYFKN2YSPT7VJA', 
-    :aws_secret_access_key  => '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', 
-  } 
-  config.fog_directory  = 'goodfornothing' 
-  config.fog_public     = false 
-end
-
 if Rails.env.test? or Rails.env.cucumber?
   CarrierWave.configure do |config|
     config.storage = :file
@@ -23,6 +13,13 @@ end
 
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.storage = :s3
+    config.storage = :fog
+    config.fog_credentials = { 
+      :provider               => 'AWS', 
+      :aws_access_key_id      => 'AKIAIWNYFKN2YSPT7VJA', 
+      :aws_secret_access_key  => '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', 
+    } 
+    config.fog_directory  = 'goodfornothing' 
+    config.fog_public     = false
   end
 end

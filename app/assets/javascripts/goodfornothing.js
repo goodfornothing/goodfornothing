51ab76f2e88f449e766c9384d14f532e65849145
Andrew Sprinz <andrew@phenotypic.co.uk>
2012-09-15 10:51:01 +0100

    Betterer js form labels

--- app/assets/javascripts/goodfornothing.js
+++ app/assets/javascripts/goodfornothing.js
@@ -3,7 +3,9 @@ $(document).ready(function(){
 	// Smart Labels
 	$('.input.smart_label').each(function(i,el){
 		
-		label = $($(el).find('label')[0]).hide();
+		label = $($(el).find('label')[0]).addClass('js');
+		
+		label.text(label.text().replace('*',''))
 		
 		if($(el).hasClass('text')) {
 			input = $($(el).find('textarea')[0]);
@@ -11,36 +13,17 @@ $(document).ready(function(){
 			input = $($(el).find('input')[0]);
 		}
 		
-		text = label.text().replace('* ','');
-		input.data('placeholder', text);
-		
-		if(input.val()=='') { input.val(text); } 
-		
 		input.bind('focus',function(ev){
-			if($(this).val()==$(this).data('placeholder')) {
-				$(this).val('')
-			}
+			$($(this).siblings('label')[0]).hide()
 		});
 		
 		input.bind('blur',function(ev){
 			if($(this).val()=='') {
-				$(this).val($(this).data('placeholder'));
+				$($(this).siblings('label')[0]).show()
 			}
 		});
 		
 	});
-	
-	$('form').each(function(i,el){
-
-		$(el).bind('submit',function(ev){
-			$(el).find('.smart_label input, .smart_label textarea').each(function(i,input) {
-				if($(input).val()==$(input).data('placeholder')) {
-					$(input).val('');
-				}
-			})
-		})
-		
-	})
 	
 	// Range sliders
 	$('input[type="range"]').rangeinput();

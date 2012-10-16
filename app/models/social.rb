6d079770b04efbd215ecfbaea39a939bac16b807
Andrew Sprinz <andrewsprinz@gmail.com>
2012-10-16 15:07:06 +0100

    Social comments

--- app/models/social.rb
+++ app/models/social.rb
@@ -18,7 +18,7 @@ class Social < ActiveRecord::Base
   friendly_id :start_time, use: :history
 	just_define_datetime_picker :start_time, :add_to_attr_accessible => true
 	
-  acts_as_commentable
+  acts_as_commentable, :title => "What's your Better Problem?", :label => "Problem"
 
 	def create_generic_slot
 	  Slot.create!(:social_id => self.id)

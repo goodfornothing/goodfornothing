c1ea72b506a03844f9e883e5614f6f13b7a55616
Andrew Sprinz <andrewsprinz@gmail.com>
2012-12-05 18:33:44 +0000

    DMing

---
+++ db/migrate/20121205162318_add_private_to_profiles.rb
@@ -0,0 +1,5 @@
+class AddPrivateToProfiles < ActiveRecord::Migration
+  def change
+		add_column :users, :private, :boolean, :default => false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121205144226) do
+ActiveRecord::Schema.define(:version => 20121205162318) do
 
   create_table "challenges", :force => true do |t|
     t.string  "title"
@@ -307,6 +307,7 @@ ActiveRecord::Schema.define(:version => 20121205144226) do
     t.boolean  "subscribed",             :default => false
     t.string   "role"
     t.boolean  "can_be_contacted",       :default => true
+    t.boolean  "private",                :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true

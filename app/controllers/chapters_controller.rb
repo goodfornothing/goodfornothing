2604acd0c8ac788a45743463b65859684397d8a6
Tom Martin <tom@383project.com>
2014-01-23 17:26:52 +0000

    Sorted previous events in the right order

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -3,7 +3,7 @@ class ChaptersController < ApplicationController
   def show
     
       @chapter = Chapter.find_by_slug(params[:id])
-            
+
       @socials = Social.where('start_time >= ?',Time.now).where('chapter_id = ?',@chapter.id).order("start_time DESC")
       @gigs = Gig.where('start_time >= ?',Time.now).where('chapter_id = ?',@chapter.id).order("start_time DESC")      
 
@@ -11,7 +11,7 @@ class ChaptersController < ApplicationController
       @previous_gigs = Gig.where('start_time < ?',Time.now).where('chapter_id = ?',@chapter.id).order("start_time DESC")      
       
       @upcoming_events = (@socials + @gigs).sort_by(&:start_time)
-      @previous_events = (@previous_socials + @previous_gigs).sort_by(&:start_time)
+      @previous_events = (@previous_socials + @previous_gigs).sort_by(&:start_time).reverse
     
   end
 

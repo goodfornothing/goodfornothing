2efeae6b2caf8714a0ae0c00ea1a5dbac6299325
Tom Martin <tom@383project.com>
2014-03-07 11:12:57 +0000

    added /shop as another redirect for the store

--- config/routes.rb
+++ config/routes.rb
@@ -107,6 +107,7 @@ Goodfornothing::Application.routes.draw do
   match "datums" => "colophon#datums"
   match "survey" => redirect("https://swarm.typeform.com/to/A3gBQ3?embed=full"), :as => :store    
   match "store" => redirect("http://goodfornothing.ourtshirtshop.co.uk/"), :as => :store  
+  match "shop" => redirect("http://goodfornothing.ourtshirtshop.co.uk/"), :as => :store    
   
 	match 'good-in-your-hood' => "colophon#chapter", :as => "chapter_register"
    

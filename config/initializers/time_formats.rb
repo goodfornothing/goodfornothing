58c15ba1479bae7bd9ff154c5272299ad7e98a5d
Tom Martin <tom@383project.com>
2014-03-10 16:42:43 +0000

    added quick "add to calendar" widget via addthisevent.com

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,1 +1,2 @@
 Time::DATE_FORMATS[:article] = lambda { |time| time.strftime( "%A the #{time.day.ordinalize} of %B %Y") }
+Time::DATE_FORMATS[:ical] = lambda { |time| time.strftime( "%d-%m-%Y %H:%M:00") }

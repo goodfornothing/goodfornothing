94d8ce15fcaa81408a1cc584fa64e49f08eb0c77
Tom Martin <tom@383project.com>
2014-03-12 18:27:04 +0000

    strip config initializers

---
+++ config/initializers/stripe.rb
@@ -0,0 +1,5 @@
+# load stripe config
+yaml_file = File.join(Rails.root, "config", "stripe.yml")
+stripe_info = YAML.load_file(yaml_file)[Rails.env.to_s]
+ENV['STRIPE_SECRET_KEY'] = stripe_info['secret_key']
+ENV['STRIPE_PUBLISHABLE_KEY'] = stripe_info['publishable_key']

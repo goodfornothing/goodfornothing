615c60c5b38def19e77d9f63d9a9e52d48e3d813
Ed Ball <info@aptary.com>
2014-11-18 11:44:08 +0000

    Add chapters and gigs & socials counter

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,8 @@ class HomeController < ApplicationController
 	  
 	  @member_count = User.count
 	  @venture_count = Venture.count
+	  @event_count = Social.count + Gig.count
+	  @chapter_count = Chapter.count
 	  
 	  @update = Post.published.order("created_at DESC").take(6)    
 	  @social = Social.where('start_time > ?',Time.now).order("start_time ASC").take(6)
